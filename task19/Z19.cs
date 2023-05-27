cb315665d61c54d5c6a0af7e2be6bd0f814f4fd5
KorEvgeniyfoGB <Bagz.bann4-l011a.Bann4@yandex.by>
2023-05-27 11:53:30 +0300

    add task19

---
+++ task19/Z19.cs
@@ -0,0 +1,36 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    string num = Console.ReadLine()!;
+    if ((int.TryParse(num, out int numInt)) == false)
+    {
+        Console.WriteLine("Это не число!");
+    }
+    return numInt;
+}
+
+void Palindrome(int num)
+{
+    if (num < 10000)
+    {
+        Console.WriteLine("Число не пятизначное!");
+    }
+    else
+    {
+        int firstDigit = num / 10000;
+        int secondDigit = num / 1000 % 10;
+        int fourthDigit = num / 10 % 10;
+        int lastDigit = num % 10;    
+        if (firstDigit == lastDigit && secondDigit == fourthDigit)
+        {
+            Console.WriteLine("Число палиндром");
+        }
+
+        else
+        {
+        Console.WriteLine("Число не палиндром");
+        }
+    }
+}
+int number = Prompt("Введите пятизначное число: ");
+Palindrome(number);
