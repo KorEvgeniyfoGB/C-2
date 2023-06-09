ddb3d2eafd2e0f17ac927f892317f1313cd17e98
KorEvgeniyfoGB <Bagz.bann4-l011a.Bann4@yandex.by>
2023-06-09 21:49:27 +0300

    task from sem6

---
+++ task48/Z48.cs
@@ -0,0 +1,35 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++) // GetLength(0) отвечает за строки 
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = Prompt("Строки:");
+int i = Prompt("Столбцы");
+PrintArray(GetArray(row, i));
